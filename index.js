db2953e546f6d2abd48b5a78b110936fdeeeb1df
dhruvidesai20 <71321899+dhruvidesai20@users.noreply.github.com>
2021-03-20 12:55:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-  var navLink= $(".scroll");
+ var navLink= $(".scroll");
 
   navLink.on("click", function(e){
     e.preventDefault();
@@ -6,20 +6,77 @@
       scrollTop: $(this.hash).offset().top - 110
     }, 1250)
 
+    $(this).parent().addClass('activeLink');
+    $(this).parent().siblings().removeClass('activeLink');
+
   })
 
+  function fullPageAnimation(){
+    if ($(window).width() < 426){
+      document.body.classList.remove("fullscreen");
 
-  $(window).on("scroll", function(){
-    var scrollBarLocation= $(this).scrollTop();
+      $(window).on("scroll", function(){
+        var scrollBarLocation= $(this).scrollTop();
 
-    navLink.each(function(){
-      var sectionOffset= $(this.hash).offset().top - 130;
+        navLink.each(function(){
+          var sectionOffset= $(this.hash).offset().top - 130;
 
-      if ( sectionOffset <= scrollBarLocation ) {
-        $(this).parent().addClass('activeLink');
-        $(this).parent().siblings().removeClass('activeLink');
-      }
-    })
-    })
+          if ( sectionOffset <= scrollBarLocation ) {
+            $(this).parent().addClass('activeLink');
+            $(this).parent().siblings().removeClass('activeLink');
+          }
+        })
+        })
+    }else{
 
-   
+      document.body.classList.add("fullscreen");
+
+      if(document.body.classList.contains("fullscreen")){
+
+        const sections= document.querySelectorAll("section");
+        const content= document.querySelector(".main-content");
+        const links= document.querySelectorAll(".nav-item");
+
+        let spinValue= 0;
+        let canScroll= true;
+
+        window.addEventListener("mousewheel", function(e){
+
+          if(canScroll){
+            canScroll= false;
+            if(e.deltaY >0){
+              if(spinValue< sections.length-1){
+                  spinValue +=1;
+              }
+            }else{
+              if(spinValue>0){
+                spinValue -=1;
+              }
+            }
+
+            scrollContent(spinValue);
+          }
+
+          setTimeout(function(){
+            canScroll=true;
+          }, 560);
+        })
+
+        function scrollContent(count){
+          content.setAttribute("style", "transform: translateY(-" + count*100+ "vh)");
+
+          document.querySelector(".activeLink").classList.remove("activeLink");
+          links[count].classList.add("activeLink");
+        }
+
+
+    }
+
+
+    }
+
+  }
+
+window.addEventListener("load", fullPageAnimation);
+
+window.addEventListener("resize", fullPageAnimation);
